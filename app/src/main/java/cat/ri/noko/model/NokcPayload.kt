a422576e3c02525a3075009043c094119440af76
rii <vanish-relay@pm.me>
2026-03-24 18:35:19 +0300

    Update .nokc serialization

--- app/src/main/java/cat/ri/noko/model/NokcPayload.kt
+++ app/src/main/java/cat/ri/noko/model/NokcPayload.kt
@@ -3,7 +3,18 @@ package cat.ri.noko.model
 import kotlinx.serialization.Serializable
 
 @Serializable
+data class NokcCharacter(
+    val entry: PersonaEntry,
+    val avatarBase64: String? = null,
+)
+
+@Serializable
 data class NokcPayload(
+    val characters: List<NokcCharacter>,
+)
+
+@Serializable
+data class NokcPayloadLegacy(
     val entry: PersonaEntry,
     val avatarBase64: String? = null,
 )
